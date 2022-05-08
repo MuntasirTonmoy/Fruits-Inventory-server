1c542388ebf15376499f58f86db8202b004e9d21
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-05-08 19:57:32 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const res = require("express/lib/response");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yfl4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("mongodg connected");
+  // perform actions on the collection object
+  client.close();
+});
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-const app = express();
-const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Server is running.");
