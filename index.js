b170f9026ed132b1588058fa5ec3b1a93ec949df
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-05-09 20:27:14 +0600

    update added

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const res = require("express/lib/response");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yfl4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("mongodg connected");
-  // perform actions on the collection object
-  client.close();
-});
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,6 +10,58 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yfl4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const fruitsCollection = client
+      .db("fruitsInventory")
+      .collection("fruitsCollection");
+
+    //get the product
+    app.get("/fruits", async (req, res) => {
+      const query = {};
+      const cursor = fruitsCollection.find(query);
+      const fruits = await cursor.toArray();
+      res.send(fruits);
+    });
+
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await fruitsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const newQuantity = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateQuantity = {
+        $set: {
+          quantity: newQuantity.quantity,
+        },
+      };
+      const result = await fruitsCollection.updateOne(
+        filter,
+        updateQuantity,
+        options
+      );
+      res.send(result);
+    });
+  } finally {
+  }
+};
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running.");
